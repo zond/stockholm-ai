e4f132199e3b46e5b39b84acc69f8ca4b8733922
Martin Bruse <zondolfin@gmail.com>
2021-02-02 14:20:59 +0100

    ..

--- hub/web/web.go
+++ hub/web/web.go
@@ -23,13 +23,13 @@ import (
 	simpletonAi "github.com/zond/stockholm-ai/simpleton/ai"
 )
 
-var htmlTemplates = template.Must(template.New("htmlTemplates").ParseGlob("templates/html/*.html"))
-var jsModelTemplates = template.Must(template.New("jsModelTemplates").ParseGlob("templates/js/models/*.js"))
-var jsCollectionTemplates = template.Must(template.New("jsCollectionTemplates").ParseGlob("templates/js/collections/*.js"))
-var jsViewTemplates = template.Must(template.New("jsViewTemplates").ParseGlob("templates/js/views/*.js"))
-var _Templates = template.Must(template.New("_Templates").ParseGlob("templates/_/*.html"))
-var jsTemplates = template.Must(template.New("jsTemplates").ParseGlob("templates/js/*.js"))
-var cssTemplates = template.Must(template.New("cssTemplates").ParseGlob("templates/css/*.css"))
+var htmlTemplates = template.Must(template.New("htmlTemplates").ParseGlob("hub/templates/html/*.html"))
+var jsModelTemplates = template.Must(template.New("jsModelTemplates").ParseGlob("hub/templates/js/models/*.js"))
+var jsCollectionTemplates = template.Must(template.New("jsCollectionTemplates").ParseGlob("hub/templates/js/collections/*.js"))
+var jsViewTemplates = template.Must(template.New("jsViewTemplates").ParseGlob("hub/templates/js/views/*.js"))
+var _Templates = template.Must(template.New("_Templates").ParseGlob("hub/templates/_/*.html"))
+var jsTemplates = template.Must(template.New("jsTemplates").ParseGlob("hub/templates/js/*.js"))
+var cssTemplates = template.Must(template.New("cssTemplates").ParseGlob("hub/templates/css/*.css"))
 
 func allCSS(c common.Context) {
 	c.SetContentType("text/css; charset=UTF-8", true)
@@ -230,7 +230,7 @@ func handleStatic(router *mux.Router, dir string) {
 			} else {
 				common.SetContentType(w, "application/octet-stream", true)
 			}
-			if in, err := os.Open(filepath.Join("static", cpy)); err != nil {
+			if in, err := os.Open(filepath.Join("hub/static", cpy)); err != nil {
 				w.WriteHeader(500)
 			} else {
 				defer in.Close()
@@ -280,7 +280,7 @@ func main() {
 	router.Path("/examples/simpleton").Methods("POST").Handler(ai.HTTPHandlerFunc(common.GAELoggerFactory, simpletonAi.Simpleton{}))
 	router.Path("/examples/broken").Methods("POST").Handler(ai.HTTPHandlerFunc(common.GAELoggerFactory, brokenAi.Broken{}))
 
-	handleStatic(router, "static")
+	handleStatic(router, "hub/static")
 
 	router.PathPrefix("/").MatcherFunc(wantsHTML).HandlerFunc(handler(index))
 	http.Handle("/", router)
